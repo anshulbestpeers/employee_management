2f6fe68b1b3f26a0bd5c071a91fa32e46c5e745f
Anshul Agrawal <anagrawal@bestpeers.com>
2023-07-06 16:35:16 +0530

    Added all action of employees_controller

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -3,4 +3,40 @@ class EmployeesController < ApplicationController
     @employees = Employee.all
     render json: {employees: @employees}
   end
+
+  def show
+    employee = Employee.find(params[:id])
+    render json: employee
+  end
+
+  def create
+    employee = Employee.new(employee_params)
+    if employee.save
+      render json: employee, status: :created
+    else
+      render json: employee.errors, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    employee = Employee.find(params[:id])
+
+    if employee.update(employee_params)
+      render json: employee
+    else
+      render json: employee.errors, status: :unprocessable_entity
+    end
+  end
+  
+  def destroy 
+    employee = Employee.find(params[:id])
+    employee.destroy
+    head :no_content
+  end
+
+  private
+
+  def employee_params
+    params.require(:employee).permit(:name, :email, :password, :dob, :qualification, :contact, :gender)
+  end  
 end
