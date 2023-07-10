6698061775f92e631c19e316399ff67dc2473a00
Anshul Agrawal <anagrawal@bestpeers.com>
2023-07-10 15:29:34 +0530

    Added designation with employee

--- app/serializers/employee_serializer.rb
+++ app/serializers/employee_serializer.rb
@@ -1,7 +1,11 @@
 class EmployeeSerializer < ActiveModel::Serializer
-  attributes :id, :name, :email, :department
+  attributes :id, :name, :email, :department, :designation
 
   def department
     object.department.name
+  end
+
+  def designation
+    object.designation.name
   end
 end
