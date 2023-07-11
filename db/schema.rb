d0d8bb828ee89621efacbb59a4e094b331c6d3f7
Anshul Agrawal <anagrawal@bestpeers.com>
2023-07-11 13:47:47 +0530

    Changed database to postgres

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2023_07_10_123037) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "departments", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -36,8 +39,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_10_123037) do
     t.string "qualification"
     t.string "contact"
     t.string "gender"
-    t.integer "department_id", null: false
-    t.integer "designation_id", null: false
+    t.bigint "department_id", null: false
+    t.bigint "designation_id", null: false
     t.index ["department_id"], name: "index_employees_on_department_id"
     t.index ["designation_id"], name: "index_employees_on_designation_id"
     t.index ["email"], name: "index_employees_on_email", unique: true
@@ -45,8 +48,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_10_123037) do
   end
 
   create_table "employees_tasks", force: :cascade do |t|
-    t.integer "task_id", null: false
-    t.integer "employee_id", null: false
+    t.bigint "task_id", null: false
+    t.bigint "employee_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["employee_id"], name: "index_employees_tasks_on_employee_id"
@@ -62,7 +65,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_10_123037) do
     t.string "blood_group"
     t.string "present_address"
     t.string "permanent_address"
-    t.integer "employee_id", null: false
+    t.bigint "employee_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["employee_id"], name: "index_personal_infos_on_employee_id"
